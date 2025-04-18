a2430aa17163d30448687183ffa63a8ea82f91c2
shafqaat <rajashajaatali@gmail.com>
2025-04-18 23:22:40 +0500

    no theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 
-import { useState } from 'react'
+
 import './App.css'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Home from './Pages/Home'
-import { ThemeProvider } from 'styled-components'
-// import {lightTheme , darkTheme} from '../src/Styles/theme.js'
 
 function App() {
-  const [theme, setTheme] = useState('dark');
-
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
   return (
     <>
+      <Header />
+      <Home />
+      <Footer />
 
-    
-
-
-        <Header />
-        <Home />
-        <Footer />
-   
     </>
   )
 }
